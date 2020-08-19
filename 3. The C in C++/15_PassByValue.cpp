2889ef5fcf9d1532cdfe78175947a1a406ee655e
Vuean <hzhlxy@outlook.com>
2020-08-19 08:19:25 +0800

    feat(chapter3): add 15;

---
+++ 3. The C in C++/15_PassByValue.cpp
@@ -0,0 +1,21 @@
+// C03: 15_PassByValue.cpp
+
+#include <iostream>
+using namespace std;
+
+void f(int a)
+{
+    cout << "a= " << a << endl;
+    a = 5;
+    cout << "a= " << a << endl;
+}
+
+int main()
+{
+    int x = 47;
+    cout << "x= " << x << endl;
+    f(x);
+    cout << "x= " << x << endl;
+    return 0;
+}
+
